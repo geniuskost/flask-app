289fdc25c9138aef3005b7296719cb50d588cc4e
geniuskost <geniuskost@gmail.com>
2025-06-25 23:26:04 +0300

    NEW APP!

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
-from flask import Flask, request, jsonify
+from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/api/hello', methods=['GET'])
-def hello():
-    name = request.args.get('name', 'World')
-    return jsonify(message=f'Hello, {name}!')
+@app.route('/')
+def home():
+    return "Hello, World!"
